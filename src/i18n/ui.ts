8abe6c7312d3dc050f88cc6553ef105bc311c488
Matias Stefanutti <mstefanutti24@gmail.com>
2024-11-10 19:55:04 +0100

    feat: add i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.about': 'About',
+    'nav.schedule': 'Schedule',
+    'nav.diversity': 'Diversity',
+    'nav.sponsors': 'Sponsors',
+  },
+  es: {
+    'nav.about': 'Acerca de',
+    'nav.schedule': 'Horario',
+    'nav.diversity': 'Diversidad',
+    'nav.sponsors': 'Patrocinadores',
+  },
+} as const;
